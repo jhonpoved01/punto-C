a88642b3b7019eabf3435e62908e8adc51d704d7
jhonpoved01 <sjpp5506@gmail.com>
2026-02-27 16:57:19 -0500

    se realiza el punto 7

---
+++ js/Calcular_doble_ triple.js
@@ -0,0 +1,13 @@
+
+let numero = Number(prompt("Por favor, ingresa un número:"));
+
+
+let doble = numero * 2;
+let triple = numero * 3;
+
+
+console.log("El doble de " + numero + " es: " + doble);
+console.log("El triple de " + numero + " es: " + triple);
+
+
+alert("Doble: " + doble + "\nTriple: " + triple);
